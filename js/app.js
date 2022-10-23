70cd84174c7dcf14f3664a92ce266d259ad789fe
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2022-10-23 14:37:54 -0600

    Creación función Ordenar

--- js/app.js
+++ js/app.js
@@ -38,8 +38,29 @@ function calcular(){
         // regresar el valor
         return pares;
     }
-    
+
     // Imprimir el resultado
     document.getElementById('par').value = pares(arregloAleatorio);
+
+    // 3. Diseñe una función que reciba como argumento un arreglo de 20 valores
+    // numéricos enteros, ordene los valores del arreglo de mayor a menor.
+    function ordenar(arregloAleatorio){
+        let arregloOrdenado = arregloAleatorio.slice();
+			return arregloOrdenado.sort(function(a, b) {return b - a});
+
+    }
+
+    // imprimir los arreglos
+    function imprimirArreglos(array, etiqueta){
+        let contenido ="";
+
+        for(let i=0; i< array.length; i++){
+            contenido += "<p>"+ array[i] +"</p>"
+        }
+        etiqueta.innerHTML=contenido;
+    }
+    let arregloOrdenado = ordenar(arregloAleatorio);
+    imprimirArreglos(arregloAleatorio, document.getElementById('Normal'));
+    imprimirArreglos(arregloOrdenado, document.getElementById('Ordenado'));
 }
 
