3ada6d566389e1e3e12ea1257d14d35a55dae707
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2022-10-23 14:18:36 -0600

    Creación función pares

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ function calcular(){
         arregloAleatorio.push(Math.floor(Math.random() * (100 -1)+1));
     }
 
-    // Obtener el promedio del arreglo
+    // 1. Diseña una función que reciba como argumento un arreglo de valores
+    // enteros de 20 posiciones , regrese el valor promedio de los elementos del arreglo.
     function promedio(arregloAleatorio){
         let promedio = 0;
 
@@ -20,5 +21,25 @@ function calcular(){
     }
     // Imprimir el resultado
     document.getElementById('pr').value = promedio(arregloAleatorio);
+
+    // 2. Diseñe una función que reciba como argumento un arreglo de 20 valores
+    //numéricos enteros, y me regrese la cantidad de valores pares que existe en el
+    //arreglo
+    function pares(arregloAleatorio){
+        let pares = 0;
+        // Recorrer los elementos del arreglo
+        for(let i=0; i<arregloAleatorio.length; i++){
+            // comprobar si el número es par
+            if(arregloAleatorio[i]%2 == 0){
+                pares++;
+            }
+            
+        }
+        // regresar el valor
+        return pares;
+    }
+    
+    // Imprimir el resultado
+    document.getElementById('par').value = pares(arregloAleatorio);
 }
 
