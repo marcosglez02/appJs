fae02525e95fd441e514549de9fc39e334deb8dd
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2022-10-23 14:05:11 -0600

    Creación función promedio

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-// Generación de un arreglo aleatorio
-const arregloAleatorio = []
-for (let i = 0; i<20; i++){
-    arregloAleatorio.push(Math.floor(Math.random() * (100 -1)+1))
+// Función de generar el arreglo
+function calcular(){
+    // Generación de un arreglo aleatorio
+    const arregloAleatorio = [];
+    for (let i = 0; i<20; i++){
+        arregloAleatorio.push(Math.floor(Math.random() * (100 -1)+1));
+    }
+
+    // Obtener el promedio del arreglo
+    function promedio(arregloAleatorio){
+        let promedio = 0;
+
+        // Recorrer los elementos del arreglo
+        for(let i=0; i<arregloAleatorio.length; i++){
+            promedio = promedio + arregloAleatorio[i];
+        }
+
+        // regresar el valor
+        return promedio/arregloAleatorio.length ;
+    }
+    // Imprimir el resultado
+    document.getElementById('pr').value = promedio(arregloAleatorio);
 }
+
